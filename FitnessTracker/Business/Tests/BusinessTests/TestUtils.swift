6b73c377a45feb0ab20f08f491efb6dbe115b361
Inal Gotov <sketch204@gmail.com>
2024-01-14 14:15:47 -0500

    fixup: 5ca5a9f159ad41a51f563e

--- FitnessTracker/Business/Tests/BusinessTests/TestUtils.swift
+++
@@ -1,9 +0,0 @@
-import Business
-
-extension Exercise: Equatable {
-    public static func == (lhs: Exercise, rhs: Exercise) -> Bool {
-        lhs.id == rhs.id
-        && lhs.name == rhs.name
-        && lhs.description == rhs.description
-    }
-}
