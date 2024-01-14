7845f1ea435e41a990934883a95c7ddc9c90063f
Inal Gotov <sketch204@gmail.com>
2024-01-14 13:42:54 -0500

    Add the ability to delete exercises

--- FitnessTracker/Views/Exercise/ExerciseListView.swift
+++ FitnessTracker/Views/Exercise/ExerciseListView.swift
@@ -6,10 +6,20 @@ struct ExerciseListView: View {
     
     @State private var isAddingExercise: Bool = false
     
+    @State private var isProposingDeletion: Bool = false
+    @State private var proposedExerciseIdDeletion: Exercise.ID?
+    
     var body: some View {
         List {
             ForEach(store.exercises) { exercise in
                 ExerciseRow(exercise)
+                    .swipeActions(allowsFullSwipe: false) {
+                        Button("Delete") {
+                            proposedExerciseIdDeletion = exercise.id
+                            isProposingDeletion = true
+                        }
+                        .tint(.red)
+                    }
             }
         }
         .navigationTitle("Exercises")
@@ -25,6 +35,22 @@ struct ExerciseListView: View {
                 EditExerciseView(store: store)
             }
         }
+        .confirmationDialog(
+            "Are you sure you want to delete this exercise?",
+            isPresented: $isProposingDeletion,
+            presenting: proposedExerciseIdDeletion) { id in
+                Button("Cancel", role: .cancel) {
+                    proposedExerciseIdDeletion = nil
+                }
+                
+                Button("Delete", role: .destructive) {
+                    store.removeExercise(id: id)
+                    proposedExerciseIdDeletion = nil
+                }
+            } message: { _ in
+                Text("This action cannot be undone")
+            }
+
     }
 }
 
