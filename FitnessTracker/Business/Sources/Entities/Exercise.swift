d87e0be45250448086543b1991d0acdba23650d3
Inal Gotov <sketch204@gmail.com>
2024-01-14 12:45:27 -0500

    Add an `Exercise` type

---
+++ FitnessTracker/Business/Sources/Entities/Exercise.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+public struct Exercise: Identifiable {
+    public let id: Identifier<Self, UUID>
+    public let name: String
+    public let description: String?
+    
+    public init(id: UUID, name: String, description: String?) {
+        self.id = Identifier(rawValue: id)
+        self.name = name
+        self.description = description
+    }
+}
